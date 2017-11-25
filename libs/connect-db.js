b6f0833fa5165484a3665f82043c51d8562abf46
Vinícius Miiller Rebello <miiller.vini@gmail.com>
2017-11-25 15:39:14 -0200

    Tirado código comentado

--- libs/connect-db.js
+++ libs/connect-db.js
@@ -8,7 +8,6 @@ module.exports = function(){
 		const DATABASE = 'demerge';
 		mongoose.Promise = global.Promise;
 		db = mongoose.connect('mongodb://127.0.0.1:27017/demerge');
-		// db = mongoose.connect(`mongodb://raffa:${PASSWORD}@raffamepaga-shard-00-00-npq09.mongodb.net:27017,raffamepaga-shard-00-01-npq09.mongodb.net:27017,raffamepaga-shard-00-02-npq09.mongodb.net:27017/${DATABASE}?ssl=true&replicaSet=raffamepaga-shard-0&authSource=admin`);
 	}
 	return db;
 }
