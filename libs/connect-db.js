0cd85b73cb759599a051d478ac84ee1cc14641f4
viniceosm <miiller.vini@gmail.com>
2018-01-13 15:51:09 -0200

    Trocado bd

--- libs/connect-db.js
+++ libs/connect-db.js
@@ -4,10 +4,11 @@ var db;
 
 module.exports = function(){
 	if(!db){
-		const PASSWORD = 'insta';
+		const PASSWORD = 'demergetrap';
 		const DATABASE = 'demerge';
 		mongoose.Promise = global.Promise;
-		db = mongoose.connect('mongodb://127.0.0.1:27017/demerge');
+		db = mongoose.connect(`mongodb://demerge:${PASSWORD}@demerge-shard-00-00-f8plz.mongodb.net:27017,demerge-shard-00-01-f8plz.mongodb.net:27017,demerge-shard-00-02-f8plz.mongodb.net:27017/${DATABASE}?ssl=true&replicaSet=demerge-shard-0&authSource=admin`);
+		// db = mongoose.connect('mongodb://127.0.0.1:27017/demerge');
 	}
 	return db;
 }
