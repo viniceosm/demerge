fa6e260b44f653de32b1e83780b8fc4b6acb0eb7
viniceosm <miiller.vini@gmail.com>
2018-01-23 01:41:19 -0200

    Alterando port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // require('./minificarArquivos')();
 
